dfc66df183807a400ec5b0bc66c68d89459a5cb3
Taz <tanzim_zaman@hotmail.com>
2016-10-15 22:57:14 -0400

    parser donesque

--- Leos/src/Parser.java
+++ Leos/src/Parser.java
@@ -12,9 +12,42 @@ import org.apache.poi.xwpf.usermodel.XWPFTableCell;
 import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
 public class Parser {
+	
+	
+	public ArrayList<Employee> parseEmployee() throws IOException {
+		ArrayList<Employee> employees = new ArrayList<>();
+		FileInputStream fis = new FileInputStream("./Programming Employee List (1).docx");
+		String tempName = "";
+		String  tempDate = "";
+		String test="";
+		int n = -1;
+		
+		XWPFDocument doc = new XWPFDocument(fis);
+		List<XWPFTable> tables = doc.getTables();
+		   for ( XWPFTable table : tables ) {
+               for ( XWPFTableRow row : table.getRows() ) { 
+                   for ( XWPFTableCell cell : row.getTableCells()) {
+                	   test = cell.getText();
+                	   if(n >= 1) {
+                		   if(n == 1) {
+                    		   tempName = cell.getText();   	   
+                    	   } else {
+                    		   n = 0;
+                    		   tempDate = cell.getText();
+                    	   }
+                   
+                	   }
+                	   n++;
+            	   }
+               employees.add(new Employee(tempName,tempDate));
+                   
+               }
+		   }
+		   return employees;
+           }
 
 	
-	public ArrayList<Item> parse() throws IOException{
+	public ArrayList<Item> parseItems() throws IOException{
 		ArrayList<Item> items = new ArrayList<>();
 		FileInputStream fis = new FileInputStream("./Programming Food List (1).docx");
 		String tempName = "";
@@ -51,11 +84,5 @@ public class Parser {
 		   return items;
            }
 
-		   
-	
-	public static void main(String args[]) throws IOException{
-		Parser p =new Parser();
-		p.parse();
-	}
 }
 
