0fbf1fb81272ddb6b804aa1ee47b22a80c77ceb2
dangravel <dan0gravel@gmail.com>
2016-10-15 22:44:44 -0400

    added date parser

--- Leos/src/Item.java
+++ Leos/src/Item.java
@@ -1,4 +1,13 @@
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+import java.text.SimpleDateFormat;
+
+
+
 /**
  * Items in the store
  * @author dan
@@ -10,18 +19,25 @@ public class Item {
 	private int quantity;
 	private int popularity;
 	
-	public Item(String name, LocalDate expirydate, int quantity){
+	public Item(String name, String expirydate, int quantity){
 		this(name,expirydate);
 		this.quantity = quantity;
 		this.popularity = 0;
 	}
 	
-	public Item(String name, LocalDate expirydate){
+	public Item(String name, String expirydate){
 		this.name = name;
-		this.expirydate = expirydate;
+		this.expirydate = convertDate(expirydate);
 		this.popularity = 0;
 	}
-
+	public Item(){}
+	private LocalDate convertDate(String date){
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d, yyyy");
+		formatter = formatter.withLocale(Locale.US);  
+		LocalDate d = LocalDate.parse(date, formatter);
+		return d;
+	}
+	
 	public LocalDate getExpirydate() {
 		return expirydate;
 	}
@@ -37,5 +53,11 @@ public class Item {
 	public void decrementQuantity() {
 		quantity--;
 		popularity++;
+	}
+	
+	public static void main(String args[]){
+		//Item t = new Item();
+		//LocalDate d = t.convertDate("Jun 16, 2018");
+		//System.out.println(d);
 	}
 }
