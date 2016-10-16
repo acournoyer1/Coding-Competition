972e1c2503d8a983a8747836f1df295fff23f872
Taz <tanzim_zaman@hotmail.com>
2016-10-16 01:28:12 -0400

    test

--- Leos/src/Employee.java
+++ Leos/src/Employee.java
@@ -10,73 +10,75 @@ import java.time.*;
 
 public class Employee {
 	private String name;
-	private boolean canWorkMonday = false;
-	private boolean canWorkTuesday = false;
-	private boolean canWorkWednesday = false;
-	private boolean canWorkThursday = false;
-	private boolean canWorkFriday = false;
-	private boolean canWorkSaturday = false;
-	private boolean canWorkOvernight = false;
-	//private HashMap<DayOfWeek, AvailabilityTime> availability;
-	
-	public Employee(String name, String availablility) {
-		this.name = name;
-		String[] days = availablility.split(", ");
-		for(String day: days){
-			if(day.contains("-")){
-				parseRange();
-			} else {
-				String[] b = day.split(" ");
-				parseAvailability(b);
-			}
-		}
-	}
-	
-	public void parseRange(){
-		canWorkMonday = true;
-		canWorkTuesday = true;
-		canWorkWednesday = true;
-		canWorkThursday = true;
-		canWorkFriday = true;
-	}
-	
-	public void parseAvailability(String[] a){
-		DayOfWeek day = null;
-		for (int i = 0; i < a.length; i++){
-			switch (a[i]){
-				case "Monday":
-					canWorkMonday = true;
-					break;
-				case "Tuesday":
-					canWorkTuesday = true;
-					break;
-				case "Wednesday":
-					canWorkWednesday = true;
-					break;
-				case "Thursday":
-					canWorkThursday = true;
-					break;
-				case "Friday":
-					canWorkFriday = true;
-					break;
-				case "Saturday":
-					canWorkSaturday = true;
-					break;
-				default:
-					checkOvernight(a[i]);
-					break;
-			}		
-		}
-	}
-	
-	private void checkOvernight(String a){
-		if(a.equals("overnight")){
-			canWorkOvernight = true;
-		} else if (a.equals("no")) {
-			canWorkOvernight = false;
-		}
-	}
-	
+    private boolean canWorkMonday = false;
+    private boolean canWorkTuesday = false;
+    private boolean canWorkWednesday = false;
+    private boolean canWorkThursday = false;
+    private boolean canWorkFriday = false;
+    private boolean canWorkSaturday = false;
+    private boolean canWorkOvernight = false;
+    private boolean alreadyNo = false;
+   // private HashMap<DayOfWeek, AvailabilityTime> availability;
+    
+    public Employee(String name, String availablility) {
+        this.name = name;
+        String[] days = availablility.split(", ");
+        for(String day: days){
+            if(day.contains("-")){
+                parseRange();
+            } else {
+                String[] b = day.split(" ");
+                parseAvailability(b);
+            }
+        }
+    }
+    
+    public void parseRange(){
+        canWorkMonday = true;
+        canWorkTuesday = true;
+        canWorkWednesday = true;
+        canWorkThursday = true;
+        canWorkFriday = true;
+    }
+    
+    public void parseAvailability(String[] a){
+        DayOfWeek day = null;
+        for (int i = 0; i < a.length; i++){
+            switch (a[i]){
+                case "Monday":
+                    canWorkMonday = true;
+                    break;
+                case "Tuesday":
+                    canWorkTuesday = true;
+                    break;
+                case "Wednesday":
+                    canWorkWednesday = true;
+                    break;
+                case "Thursday":
+                    canWorkThursday = true;
+                    break;
+                case "Friday":
+                    canWorkFriday = true;
+                    break;
+                case "Saturday":
+                    canWorkSaturday = true;
+                    break;
+                default:
+                    checkOvernight(a[i]);
+                    break;
+            }       
+        }
+    }
+    
+    private void checkOvernight(String a){
+        if(a.equals("overnight") && !alreadyNo){
+            canWorkOvernight = true;
+        } else if (a.equals("no")) {
+            canWorkOvernight = false;
+            alreadyNo = true;
+        }
+    }
+    
 	public boolean isCanWorkMonday() {
 		return canWorkMonday;
 	}
