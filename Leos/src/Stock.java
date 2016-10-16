f3e3750174120bf4e6bc36fc1484240fe33715a7
Daniel Gravel <dan0gravel@gmail.com>
2016-10-16 00:10:38 -0400

    k

--- Leos/src/Stock.java
+++ Leos/src/Stock.java
@@ -1,5 +1,56 @@
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Stock {
-	private ArrayList<Item> items;
+	public ArrayList<Item> items;
+	private Item topSeller;
+	
+	public Stock(){
+		this.items = new ArrayList<Item>();
+	}
+
+	public void addItem(ArrayList<Item> food){
+		items.addAll(food);
+	}
+	
+	
+	public ArrayList<Item> criticalItems(){
+		ArrayList<Item> critical = new ArrayList<Item>();
+		Date currentTime = new Date(System.currentTimeMillis());
+		int currentYear = currentTime.getYear();
+		int currentMonth = currentTime.getMonth();
+		int currentDay = currentTime.getDay();
+		
+		for(Item it: items){
+			int itYear = it.getExpirydate().getYear();
+			int itMonth = it.getExpirydate().getMonthValue();
+			
+		}
+	}
+	
+	
+	public Item getTopSeller(){
+		topSeller = items.get(0);
+		for(Item it: items){
+			if(it.getPopularity() > topSeller.getPopularity()){
+				topSeller = it;
+			}
+		}
+		return topSeller;
+	}
+	
+	
+	public static void main(String args[]) throws IOException{
+		Stock s = new Stock();
+		System.out.println("TEST");
+		
+		s.addItem(Parser.parseItems());
+		System.out.println(s.items.size());
+		for(Item i : s.items){
+			System.out.println(i);
+		}
+	}
+		
 }
