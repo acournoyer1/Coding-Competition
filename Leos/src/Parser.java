1fff59d4da714bd1ba566ff2e4e49a0638dc985b
Taz-Z <tanzim_Zaman@hotmail.com>
2016-10-15 23:44:27 -0400

    Update Parser.java
    fixed

--- Leos/src/Parser.java
+++ Leos/src/Parser.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
@@ -11,31 +13,103 @@ import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
 public class Parser {
 	
-	public Parser(){}
 	
-	public void parse() throws IOException{
-		
-		FileInputStream fis = new FileInputStream("/home/dan/workspace/Coding-Competition/Programming Food List (1).docx");
+	public ArrayList<Employee> parseEmployee() throws IOException {
+		ArrayList<Employee> employees = new ArrayList<>();
+		FileInputStream fis = new FileInputStream("./Programming Employee List (1).docx");
+		String tempName = "";
+		String  tempDate = "";
+		String test="";
+		int n = -1;
 		
 		XWPFDocument doc = new XWPFDocument(fis);
 		List<XWPFTable> tables = doc.getTables();
-		   for ( XWPFTable table : tables )
-           {
-               for ( XWPFTableRow row : table.getRows() )
-               {
-                   for ( XWPFTableCell cell : row.getTableCells() )
-                   {
-                       System.out.print(cell.getText());
-                       System.out.print("\t");
-                   }
-                   System.out.println("");
+		   for ( XWPFTable table : tables ) {
+               for ( XWPFTableRow row : table.getRows() ) { 
+                   for ( XWPFTableCell cell : row.getTableCells()) {
+                	   test = cell.getText();
+                	   if(n >= 1) {
+                		   if(n == 1) {
+                    		   tempName = cell.getText();   	   
+                    	   } else {
+                    		   n = 0;
+                    		   tempDate = cell.getText();
+                    	   }
+                   
+                	   }
+                	   n++;
+            	   }
+               employees.add(new Employee(tempName,tempDate));
+                   
                }
+		   }
+		   return employees;
            }
-	}
+
 	
-	public static void main(String args[]) throws IOException{
-		Parser p =new Parser();
-		p.parse();
+	public ArrayList<Item> parseItems() throws IOException{
+		ArrayList<Item> items = new ArrayList<>();
+		FileInputStream fis = new FileInputStream("./Programming Food List (1).docx");
+		int x = 0;
+		String tempName = "";
+		int tempQuantity = 0;
+		String  tempDate = "";
+		String test="";
+		int n = -2;
+		
+		XWPFDocument doc = new XWPFDocument(fis);
+		List<XWPFTable> tables = doc.getTables();
+		   for ( XWPFTable table : tables ) {
+               for ( XWPFTableRow row : table.getRows() ) { 
+                   for ( XWPFTableCell cell : row.getTableCells()) {
+                	   test = cell.getText();
+                	   if(n >= 1) {
+                		   if(n == 1) {
+                    		   tempName = cell.getText();   
+                    		   
+                    	   } else if(n == 2) {
+                    		   tempDate = cell.getText();
+                    		   
+                    	   } else {
+                    		   n = 0;
+                    		   tempQuantity = Integer.parseInt(cell.getText().replaceAll("[^0-9]",""));
+                    	   }
+                   
+                	   }
+                	   n++;
+            	   }
+                   if(x > 0) {
+                        int i;
+                        for(i = 0; i < tempDate.length(); i++) {
+                        	if(tempDate.charAt(i) == ' ') {
+                        		break;
+                        	}
+                        }
+                        String xmonth= tempDate.substring(i);
+                        String month = tempDate.substring(0, 3);
+                        tempDate = month + xmonth;
+
+                        
+                    items.add(new Item(tempName,tempDate , tempQuantity));
+                    
+                   }
+                   x++;
+               
+                   
+               }
+		   }
+		   return items;
+           }
+	
+	public static void main(String args[]) {
+		Parser p = new Parser();
+		try {
+			p.parseItems();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	}
+
+}
 
